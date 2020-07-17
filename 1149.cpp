b7924b1fa090cb61941a0e12cec57d140f6f0314
yj____ <60027712+YJ243@users.noreply.github.com>
2020-07-17 14:45:21 +0900

    Update 1149.cpp

--- 1149.cpp
+++ 1149.cpp
@@ -15,7 +15,7 @@ int main() {
 	}
 	
 
-	int d[1001][3]; //d[i][j]: i¹øÁıÀ» »öj·Î Ä¥ÇßÀ» ¶§ 1~i¹ø Áı Ä¥ÇÏ´Â ºñ¿ë ÃÖ¼Ú°ª
+	int d[1001][3]; //d[i][j]: ië²ˆì§‘ì„ ìƒ‰jë¡œ ì¹ í–ˆì„ ë•Œ 1~ië²ˆ ì§‘ ì¹ í•˜ëŠ” ë¹„ìš© ìµœì†Ÿê°’
 	d[0][0] = a[0][0], d[0][1] = a[0][1], d[0][2] = a[0][2];
 	for (int i = 1; i < n; i++) {
 		d[i][0] = min(d[i - 1][1], d[i - 1][2]) + a[i][0];//R
@@ -32,29 +32,3 @@ int main() {
 	
 	return 0;
 }
-
-/*
-¹éÁØ ¼Ò½ºÄÚµå
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int a[1001][3];
-int d[1001][3];
-int main(){
-    ios_base::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    for(int i=1; i<=n; i++)
-        for(int j=0; j<3; j++)
-            cin >> a[i][j];
-
-    for(int i=1;i<=n;i++){
-        d[i][0] = min(d[i-1][1], d[i-1][2]) + a[i][0];
-        d[i][1] = min(d[i-1][0], d[i-1][2]) + a[i][1];
-        d[i][2] = min(d[i-1][0], d[i-1][1]) + a[i][2];
-    }
-    cout << min({d[n][0], d[n][1], d[n][2]})<'\n';
-    return 0;
-}
-*/
